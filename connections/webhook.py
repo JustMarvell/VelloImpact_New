6ab8ae2a3c41ab0fcacab10f17a03cd824b8ae65
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-10-19 22:53:40 +0800

    modify message

--- connections/webhook.py
+++ connections/webhook.py
@@ -2,8 +2,8 @@ import requests
 import settings
 
 async def PostWebhook(json : dict):
-    result = requests.post(settings.DISCORD_WEBHOOK_URL_SECRET, json=json)
-    if 200 <= result.status_code < 300:
+    try:
+        requests.post(settings.DISCORD_WEBHOOK_URL_SECRET, json=json)
         return True
-    else:
-        return False
+    except Exception as e:
+        return e
