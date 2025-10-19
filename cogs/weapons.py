33e2c31f09eecd00e4f95037b4bc0fdba64a25f2
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-10-19 21:42:12 +0800

    added weapon function

---
+++ cogs/weapons.py
@@ -0,0 +1,93 @@
+from discord.ext import commands
+from discord import app_commands
+import controllers.weapons as wc
+import discord
+import typing
+import connections.webhook as wb
+import random
+
+async def setup(bot : commands.Bot):
+    await bot.add_cog(Weapons(bot)) 
+
+class Weapons(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    async def weapon_autocomplete(
+        self,
+        ctx : commands.Context,
+        current : str
+    ) -> typing.List[app_commands.Choice[str]]:
+        data = []
+        weaponlist = await wc.get_weapon_list()
+        weaponlist.append('random')
+        for weapon in weaponlist:
+            if current.lower() in weapon.lower():
+                data.append(app_commands.Choice(name = weapon, value = weapon))
+        return data[:25]
+    
+    @commands.hybrid_command()
+    @app_commands.autocomplete(weapon_name = weapon_autocomplete)
+    async def show_weapon(self, ctx : commands.Context, *, weapon_name : str):
+        """ Return a description of the selected weapon or [random] """
+        
+        if weapon_name == 'random':
+            lst = await wc.get_weapon_list()
+            c = len(lst) - 1
+            r = random.randint(0, c)
+            weapon = lst[r]
+        else:
+            weapon = weapon_name
+        
+        if weapon != None:
+            embed = discord.Embed(title = f'Genshin Impact Fandom Wiki | Weapons | {weapon_name}')
+        else:
+            await ctx.send(f'There is no weapon named {weapon_name} in the database')
+            return
+        
+        data = await wc.get_weapon_data(weapon)
+        if data == 1:
+            await ctx.send(f'No data found for {weapon}')
+            return
+        elif data == 2:
+            await ctx.send(f'Error reading data for {weapon}')
+            return
+        
+        id = data.get('id')
+        weapon_type = data.get('weapon_type')
+        quality = data.get('weapon_quality')
+        stars = await wc.convert_quality_to_star(quality)
+        base_attack = data.get('base_attack')
+        secondary_attribute_type = data.get('seconday_attribute_type')
+        secondary_attribute = data.get('secondary_attribute')
+        weapon_description = data.get('weapon_description')
+        weapon_skill_name = data.get('weapon_skill_name')
+        weapon_skill_description = data.get('weapon_skill_description')
+        weapon_icon = data.get('weapon_icon')
+        embed_color = await wc.get_color_based_on_quality(quality)
+        
+        # weapon_type = await wc.get_weapon_type(id)
+        # quality = await wc.get_weapon_quality(id)
+        # stars = await wc.convert_quality_to_star(quality)
+        # base_attack = await wc.get_weapon_base_attack(id)
+        # secondary_attribute_type = await wc.get_secondary_attribute_type(id)
+        # secondary_attribute = await wc.get_secondary_attribute(id)
+        # weapon_description = await wc.get_weapon_description(id)
+        # weapon_skill_name = await wc.get_weapon_skill_name(id)
+        # weapon_skill_description = await wc.get_weapon_skill_description(id)
+        # weapon_icon = await wc.get_weapon_icon(id)
+        # embed_color = await wc.get_color_based_on_quality(id)
+        
+        weapon_info_field = f'Name : {weapon}\nWeapon Type : {weapon_type}\nWeapon Quality : {stars}'
+        weapon_base_stats_field = f'Base ATK : {base_attack}\nSecondary Attribute : {secondary_attribute} {secondary_attribute_type}'
+        weapon_skill_field = f'{weapon_skill_description}'
+        
+        embed.description = weapon_description
+        embed.color = embed_color
+        embed.add_field(name = "WEAPON INFO", value = weapon_info_field, inline = False)
+        embed.add_field(name = "WEAPON STATS", value = weapon_base_stats_field, inline = False)
+        embed.add_field(name = f'PASSIVE SKILL : {weapon_skill_name}', value = weapon_skill_field, inline = False)
+        embed.set_image(url = weapon_icon)
+        embed.set_footer(text = "Data collected from Genshin Impact Fandom Wiki", icon_url = "https://static.wikia.nocookie.net/6a181c72-e8bf-419b-b4db-18fd56a0eb60")
+        
+        await ctx.send(embed = embed)
--- controllers/weapons.py
+++ controllers/weapons.py
@@ -1,12 +1,8 @@
 import firebase_admin, settings, glob, json, os, discord
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate(f'{settings.BASE_DIR}/key.json')
 weapon_list_dir = f'{settings.BASE_DIR}/json_data/weapon_list.json'
 
-firebase_admin.initialize_app(cred, {
-    'databaseURL' : f'{settings.FIREBASE_API_SECRET}'
-})
 
 onestarcolor = discord.Color.greyple()
 twostarcolor = discord.Color.dark_green()
@@ -14,12 +10,12 @@ threestarcolor = discord.Color.og_blurple()
 fourstarcolor = discord.Color.purple()
 fivestarcolor = discord.Color.gold()
 
-async def get_character_list():
+async def get_weapon_list():
     with open(weapon_list_dir, 'r') as f:
         data = json.load(f)
     return data
 
-async def get_weapon_data(weapon_name):
+async def get_weapon_data(weapon_name: str):
     try:
         # Sanitize character name for Firebase key
         invalid_chars = ['.', '#', '$', '/', '[', ']']
@@ -27,13 +23,39 @@ async def get_weapon_data(weapon_name):
             weapon_name = weapon_name.replace(char, '_')
             
         # Create a reference to the node "Char_[character-name]"
-        ref = db.reference(f"Weapon_{weapon_name}")
+        ref = db.reference(f"Weapon_{weapon_name.lower().capitalize()}")
         
         data = ref.get()
 
         if data:
             return data
         else:
+            print(f"No data found for {weapon_name}")
             return 1
     except Exception as e:
+        print(f"Error reading data for {weapon_name}: {e}")
         return 2
+    
+async def convert_quality_to_star(quality: int):
+    star = "⭐"
+    result = ""
+    
+    for i in range(int(quality)):
+        result += star
+    
+    return result
+
+async def get_color_based_on_quality(quality: int):
+    
+    quality = int(quality)
+    
+    if quality == 1:
+        return onestarcolor
+    elif quality == 2:
+        return twostarcolor
+    elif quality == 3:
+        return threestarcolor
+    elif quality == 4:
+        return fourstarcolor
+    elif quality == 5:
+        return fivestarcolor
