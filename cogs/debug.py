cd7b7c8a8feaebb86cb93e1ea49ccc2ae5566422
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-10-19 12:07:20 +0800

    update script

--- cogs/debug.py
+++ cogs/debug.py
@@ -12,22 +12,8 @@ class Debug(commands.Cog) :
         self.bot = bot
         
     @commands.hybrid_command()
-    async def say_hello(self, ctx : commands.Context):
-        await ctx.send("hello!")
-        
-    @commands.hybrid_command()
-    async def start(self, ctx : commands.Context):
-        """Initialize the bot"""
-        embed = discord.Embed(title="VelloImpact", description="Check status")
-        
-        embed.add_field(name="/list_commands", value="")
-        
-        embed.set_image(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTem7GDm343HtC1hOABYFfEWFrSTwyfmpDt8A&s')
-        
-        embed.set_footer(text="session date")
-        embed.timestamp = discord.utils.utcnow()
-        
-        await ctx.send(embed=embed)
+    async def say_hello(self, ctx : commands.Context, action: str):
+        await ctx.send(action)
         
     bad_ping = 15606812
     medium_ping = 15134236
@@ -51,34 +37,3 @@ class Debug(commands.Cog) :
         embed.color = ping_color
         await ctx.send(embed=embed)
         
-    @commands.hybrid_command()
-    async def list_commands(self, ctx : commands.Context):
-        """List all commands"""
-        embed = discord.Embed(title="EGG INCUBATOR", description="command list :", color=14706181)
-        
-        embed.add_field(name="/list_commands", value="list all commands", inline=False)
-        embed.add_field(name="/check_temp", value="measure the temperature inside the incubator", inline=False)
-        embed.add_field(name="/check_humidity", value="measure the humidity inside the incubator", inline=False)
-        embed.add_field(name="/toggle_light [status/on/off]", value="toggle the light based on input\n- status : Get the current state of the light (on/off)\n- on : Turn on the light\n- off : Turn off the light", inline=False)
-        embed.add_field(name="/take_photo", value="take pictures of the current conditions inside the incubator", inline=False)
-        
-        embed.set_footer(text="session date")
-        embed.timestamp = discord.utils.utcnow()
-        
-        await ctx.send(embed=embed)
-        
-    @commands.hybrid_command()
-    async def get_name(self, ctx : commands.Context, action: str = None):
-        await ctx.send("Wait for testing...")
-        
-        if action is None:
-            await ctx.send("No character name given")
-        else:
-            data = await test.get_character(action)
-            
-            await ctx.send(data)
-            
-    @commands.hybrid_command()
-    async def put_test(self, ctx : commands.Context):
-        await test.put_character()
-        await ctx.send("Put test character")
