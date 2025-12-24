3e99589bb8a11b109c9eb3858b60c97fdb39a311
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-12-24 21:55:12 +0800

    test

--- test_fix.py
+++ test_fix.py
@@ -4,6 +4,6 @@ import ipsum
 model = ipsum.load_model("en")
 
 # Returns a list of 3 strings, each resembling a paragraph of English
-paragraphs = model.generate_paragraphs(3)
+paragraphs = model.generate_paragraphs(9)
 
-print(paragraphs.pop(0))
+print(paragraphs)
