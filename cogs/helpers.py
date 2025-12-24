1d4b0afc10c98e07652d853125491f114d5813c6
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-12-24 23:05:32 +0800

    fix code, now working

--- cogs/helpers.py
+++ cogs/helpers.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import  commands
 from controllers import character_chunk
 import asyncio
+import requests
+import io
 
 model = ipsum.load_model("en")
 
@@ -33,3 +35,46 @@ class Helper(commands.Cog):
             except Exception as e:
                 await ctx.send(f"Error when sending the next paragraph, canceling proses : {e}")
                 break
+            
+    @commands.hybrid_command()
+    async def generate_qr_code(self, ctx: commands.Context, *, content: str | None, size: int = 200):
+        """ Generate a QR code from a given text 
+        Args:
+            content (text) : The content for the QR code (can be a text or a link)
+            size (100 - 600): The size of the QR code image. (default = 200)
+        """
+        if not content or content.strip() == "":
+            await ctx.send("Please provide a text for the QR code!", ephemeral=True)
+            return
+        
+        # Validate size parameter
+        if size < 100 or size > 600:
+            await ctx.send("Size must be between 100 and 1000 pixels!", ephemeral=True)
+            return
+        
+        BASE_URL = 'https://api.qrserver.com/v1/create-qr-code'
+        
+        try:
+            # this api will return an image of the qr code
+            response = requests.get(f"{BASE_URL}?data={content}&size={size}x{size}")
+
+            if response.status_code != 200:
+                await ctx.send(f"Something wrong when fetching the response! Return Code : {response.status_code}")
+                return
+            
+            # Create a BytesIO object from the image data
+            image_bytes = io.BytesIO(response.content)
+            image_bytes.seek(0)
+            
+            # Create discord.File object from the image data
+            discord_file = discord.File(image_bytes, filename="qr_code.png")
+            
+            # send the image result
+            await ctx.send("Here's your QR code!", file=discord_file)
+            
+        except Exception as e:
+            await ctx.send(f"An error occurred while generating the QR code: {e}", ephemeral=True)
+
+        
+
+        
