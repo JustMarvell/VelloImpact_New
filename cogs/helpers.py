4bba3ad7b90d0b078c6186ee8d5f3bda73712697
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-12-24 21:42:23 +0800

    add a helper function

---
+++ cogs/helpers.py
@@ -0,0 +1,34 @@
+import ipsum
+import discord
+from discord.ext import  commands
+from controllers import character_chunk
+
+model = ipsum.load_model("en")
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Helper(bot))
+
+class Helper(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    @commands.hybrid_command()
+    async def generate_random_words(self, ctx: commands.Context, *, paragraph: int):
+        """ generates a random paragraph of words
+        Args:
+             paragraph (int): the number of words to generate
+        """
+        
+        try:
+            paragraph_result = model.generate_paragraphs(paragraph)
+        except Exception as e:
+            await ctx.send(f"Error when generating the paragraph : {e}")
+            return
+        
+        chunked_paragraph = character_chunk.get_chunk(paragraph_result)
+        for p in chunked_paragraph:
+            try:
+                await ctx.send(f"{p}")
+            except Exception as e:
+                await ctx.send(f"Error when sending the next paragraph, canceling proses : {e}")
+                break
